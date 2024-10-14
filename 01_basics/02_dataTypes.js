523c7b1fb4d7506d0236a6f773bd8110022aff9b
Ashutosh Tripathi <60249551+ashutosh4036@users.noreply.github.com>
2024-10-14 07:48:44 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict";// treat all js code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+
+console.log(3 
+    +
+    3) // code readability
+
+console.log("Ashutosh")
+
+
+let name = "Ashutosh"
+let age = 22
+let isLoggedIn = true
+
+let state;
+
+// number => 2 to power 53
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
